1c884ed909d8f70140771a861300df55dce05a25
AndrewNepliakh <Father.of.people@gmail.com>
2022-05-12 20:08:17 +0300

    Implemented unloading products

--- Assets/Scripts/Controllers/PlayerController/InventoryController.cs
+++ Assets/Scripts/Controllers/PlayerController/InventoryController.cs
@@ -6,6 +6,9 @@ namespace Controllers.PlayerController
 {
     public class InventoryController : MonoBehaviour
     {
+        private const string STORAGE = "Storage";
+        private const string STOCK = "Stock";
+        
         [SerializeField] private Transform _inventory;
         private List<IProduct> _products = new List<IProduct>();
         private int _inventoryLimit = 10;
@@ -14,25 +17,43 @@ namespace Controllers.PlayerController
 
         private void OnTriggerEnter(Collider other)
         {
-            if (_products.Count >= _inventoryLimit) return;
             if (other.TryGetComponent<StorageController>(out var storage))
             {
-                var products = storage.ProductsCount;
-                if (products > _inventoryLimit) products = _inventoryLimit;
-
-                for (var i = 0; i < products; i++)
+                if (other.CompareTag(STORAGE))
                 {
-                    var position =
-                        new Vector3(_inventory.localPosition.x,
-                            _startYPos,
-                            _inventory.localPosition.z);
-                    
                     if (_products.Count >= _inventoryLimit) return;
-                    foreach (var product in storage.GetProduct(position, _inventory))
-                        _products.Add(product);
+                
+                    var products = storage.ProductsCount;
+                    if (products > _inventoryLimit) products = _inventoryLimit;
+
+                    for (var i = 0; i < products; i++)
+                    {
+                        var position =
+                            new Vector3(_inventory.localPosition.x,
+                                _startYPos,
+                                _inventory.localPosition.z);
                     
-                    _startYPos += _positionStep;
+                        if (_products.Count >= _inventoryLimit) return;
+                        foreach (var product in storage.GetProduct(position, _inventory))
+                            _products.Add(product);
+                    
+                        _startYPos += _positionStep;
+                    }
                 }
+
+                if (other.CompareTag(STOCK))
+                {
+                    for (var i = _products.Count - 1; i >= 0; i--)
+                    {
+                        if (storage.IsFull()) return;
+                        storage.SetProduct(_products[i].Transform.position, _products[i]);
+                        _products[i].Transform.SetParent(storage.transform);
+                        _products.Remove(_products[i]);
+                    }
+
+                    _startYPos = 0.0f;
+                }
+
             }
         }
     }
